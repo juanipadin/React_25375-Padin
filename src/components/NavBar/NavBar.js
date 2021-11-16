485a4ec8dbc80541938b7b81e73fa7009ab7ca24
Juan Ignacio Padin <juanipadin@hotmail.com>
2021-11-16 12:42:17 -0300

    Se completa desafío 2

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,17 @@
+import "./NavBar.css";
+
+const NavBar = () =>{
+    return (
+        <nav>
+            <ul>
+                <p>2Bike or not 2Bike</p>
+                <li><a href="#">Sobre Nosotros</a></li>
+                <li><a href="#">Productos</a></li>
+                <li><a href="#">Servicios</a></li>
+                <li><a href="#">Contacto</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar;

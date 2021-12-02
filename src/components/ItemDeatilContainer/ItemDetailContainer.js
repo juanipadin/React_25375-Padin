9ac2ebf7bc2cd0a58b66fcb76d69e6b8ab3b8d99
Juan Ignacio Padin <juanipadin@hotmail.com>
2021-12-02 01:32:45 -0300

    Se crea el loading para el ItemDetail

--- src/components/ItemDeatilContainer/ItemDetailContainer.js
+++ src/components/ItemDeatilContainer/ItemDetailContainer.js
@@ -1,9 +1,12 @@
 import React, { useEffect , useState} from 'react'
 import { useParams } from 'react-router-dom';
+import Spinner from '../../spinner/spinner';
+
 import ItemDetail from '../ItemDetail/ItemDetail';
 
 const ItemDetailContainer = () => {
     const [items, setItems] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
     const params = useParams()
 
     useEffect(()=>{
@@ -11,12 +14,13 @@ const ItemDetailContainer = () => {
             fetch(`https://api.mercadolibre.com/items/${params.id}`)
                 .then((response) => response.json())
                 .then((json) => {setItems(json)})
+                setIsLoading(false)
         }, 2000);
     },[])
 
     return (
             <div>
-                {<ItemDetail item={items}/>}
+                {isLoading ? <Spinner/> :<ItemDetail item={items}/>}
             </div>
     )
 }
